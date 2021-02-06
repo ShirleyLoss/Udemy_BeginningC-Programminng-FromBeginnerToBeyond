e220b5a0799d09028f7ef26a6eccf1e6d5f4d912
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-02-06 16:52:38 -0500

    Create Section4Challange.cpp

---
+++ Section4Challange.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+int main(){
+
+  int favoriateNumber;
+  std::cout << "Enter your favoriate number between 1 and 100:";
+  std::cin >> favoriateNumber;
+  std::cout << "Amazingn!! That's my favorite number too!";
+  std::cout << "No really!! " << favoriateNumber << " is my favoriate Number" << std::endl;
+  
+  return 0;
+  
+}
