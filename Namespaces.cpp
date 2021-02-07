d3f1bb48653be68e9521004e0567cbdc7af11841
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-02-07 00:29:27 -0500

    Create Namespaces.cpp

---
+++ Namespaces.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+
+usinng namespace std; // Use the entire std namespace
+
+/* Use only what you need. Eg:
+   using std::cout;
+   using std::cin;
+   using std::endl;
+*/
+
+int main(){
+
+  int favoriate_number;
+  
+  cout << "Enter you favorite number between 1 and 100: "
+  cin >> favoriate_number;
+  cout << "Amazing!!" << favorite_number << " is my favorite number too!" << endl;
+  
+  return 0;
+
+}
