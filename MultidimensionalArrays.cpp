9519c08dcb69576a107075bbd26677915a415f70
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-02-13 15:29:29 -0500

    Create MultidimensionalArrays.cpp

---
+++ MultidimensionalArrays.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+
+  // Element_Type array_name [dim1_size][dim2_size];
+  // dim1_size is row, dim2_size is column
+  
+  int movie_rating [3][4] 
+  {
+    {0, 4, 3, 5},
+    {2, 3, 3, 5},
+    {1, 4, 4, 5}
+  };
+  
+  return 0;
+
+}
