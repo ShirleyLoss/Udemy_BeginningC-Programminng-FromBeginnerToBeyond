efdb47e30cc8a7b2282ad278de8e3ccf7cbd9a22
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-02-14 23:17:41 -0500

    Create NeedToWork-RelationalOperators.cpp

---
+++ NeedToWork-RelationalOperators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int num1{}, num2{};
+  cout << boolalpha; // Instead of print 0 and non-zero, print False or True
+  
+  cout<< "Enter 2 integers separate" << endl;
+
+  return 0;
+
+}
